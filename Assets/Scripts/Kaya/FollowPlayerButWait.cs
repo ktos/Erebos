72b9d13380ede9a6bc0aedd4fdc1c8749a59bbde
Marcin Badurowicz <m@badurowicz.net>
2019-02-10 23:17:02 +0100

    Add floating when not following player

--- Assets/Scripts/Kaya/FollowPlayerButWait.cs
+++ Assets/Scripts/Kaya/FollowPlayerButWait.cs
@@ -3,6 +3,12 @@
 public class FollowPlayerButWait : MonoBehaviour
 {
     public GameObject player;
+    public float low = 1.4f;
+    public float high = 2.4f;
+    public float speed = 0.01f;
+
+    private Vector3 start;
+    private bool up = false;
 
     private void Update()
     {
@@ -11,6 +17,30 @@ public class FollowPlayerButWait : MonoBehaviour
         if (distance > 5)
         {
             transform.position = Vector3.Lerp(transform.position, player.transform.position - new Vector3(2, -3, 0), Time.deltaTime * 0.5f);
+
+            start = transform.position;
+        }
+        else
+        {
+            var temp = transform.position;
+            if (up)
+            {
+                temp.y += speed;
+                transform.position = temp;
+                if (transform.position.y >= high)
+                {
+                    up = false;
+                }
+            }
+            else
+            {
+                temp.y -= speed;
+                transform.position = temp;
+                if (transform.position.y <= low)
+                {
+                    up = true;
+                }
+            }
         }
     }
 }
