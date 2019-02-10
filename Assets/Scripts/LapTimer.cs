ca4f27ad20c4aa5299c9eeab3a809a9458ea2dc5
Marcin Badurowicz <m@badurowicz.net>
2019-02-10 21:34:31 +0100

    Add LapTimer for counting time of play

---
+++ Assets/Scripts/LapTimer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimer : MonoBehaviour
+{
+    public static Action<float> AfterMostRecentScoreSet;
+    private Text timeText;
+    private float gameTime = 0f;
+    private bool raceFinished = false;
+
+    private static float mostRecentScore;
+
+    public static float MostRecentScore
+    {
+        get
+        {
+            return mostRecentScore;
+        }
+
+        set
+        {
+            mostRecentScore = value;
+        }
+    }
+
+    private void Start()
+    {
+        timeText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        UpdateLapTime();
+    }
+
+    private void UpdateLapTime()
+    {
+        if (!raceFinished)
+        {
+            gameTime += Time.deltaTime;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(gameTime);
+
+            timeText.text = timeSpan.ToString(@"mm\:ss");
+        }
+    }
+
+    private void OnRaceFinished()
+    {
+        if (!raceFinished)
+        {
+            raceFinished = true;
+            MostRecentScore = gameTime;
+        }
+    }
+
+    private void OnEnable()
+    {
+        //Checkpoint.RaceFinished += OnRaceFinished;
+    }
+
+    private void OnDisable()
+    {
+        //Checkpoint.RaceFinished -= OnRaceFinished;
+    }
+}
