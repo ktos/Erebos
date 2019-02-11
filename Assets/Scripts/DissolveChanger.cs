6b4816a154137ed20e4fc1f4f40169a615bc213c
Marcin Badurowicz <m@badurowicz.net>
2019-02-11 23:32:07 +0100

    Add shaders, textures and assets

---
+++ Assets/Scripts/DissolveChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveChanger : MonoBehaviour
+{
+    private float progress = 1.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (progress <= 0.0f)
+            Destroy(this.gameObject);
+
+        //Debug.Log(progress);
+        progress -= 0.01f;
+        gameObject.GetComponent<Renderer>().material.SetFloat("_Progress", progress);
+    }
+}
