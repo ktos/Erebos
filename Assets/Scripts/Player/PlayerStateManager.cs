6cf7638e8c38d3b4105ad1fd43e3b06fc9c2cae5
Marcin Badurowicz <m@badurowicz.net>
2019-02-13 23:03:11 +0100

    Add support for torch discharge (and refactoring)

---
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    public float irritation = 0;
+    public float insanity = 0;
+}
---
+++ Assets/Scripts/Player/TorchDischarge.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchDischarge : MonoBehaviour
+{
+    public float Battery = 100.0f;
+    public float DischargeRate = 0.0001f;
+    public bool TorchEnabled = true;
+    private LightFlicker flicker;
+    private Light torch;
+
+    private void Start()
+    {
+        InvokeRepeating("Discharge", 0, 1.0f);
+        flicker = GetComponent<LightFlicker>();
+        torch = GetComponent<Light>();
+    }
+
+    private void Discharge()
+    {
+        if (!TorchEnabled)
+            return;
+
+        Battery -= DischargeRate;
+
+        if (Battery < 50)
+        {
+            flicker.StopFlickering = false;
+        }
+
+        if (Battery < 40)
+        {
+            flicker.MaxReduction = 20;
+        }
+
+        if (Battery < 30)
+        {
+            flicker.MaxReduction = 50;
+        }
+
+        if (Battery < 20)
+        {
+            flicker.MaxReduction = 100;
+        }
+
+        if (Battery < 10)
+        {
+            flicker.MaxReduction = 200;
+        }
+
+        if (Battery < 0)
+        {
+            torch.enabled = false;
+        }
+    }
+
+    public void TurnOn()
+    {
+        if (Battery <= 0)
+            return;
+
+        TorchEnabled = true;
+        torch.enabled = true;
+        flicker.enabled = true;
+    }
+
+    public void TurnOff()
+    {
+        TorchEnabled = false;
+        torch.enabled = false;
+        flicker.enabled = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (TorchEnabled)
+                TurnOff();
+            else
+                TurnOn();
+        }
+    }
+}
