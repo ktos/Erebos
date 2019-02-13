3e69a3c9205d2306900c3c0063df78c8d1af24e7
Marcin Badurowicz <m@badurowicz.net>
2019-02-14 00:58:18 +0100

    Add basic Taya spell system

---
+++ Assets/Scripts/Taya/TayaSpells.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TayaSpells : MonoBehaviour
+{
+    [Header("Taya Magic")]
+    public float Energy = 0;
+
+    public float ChargeRate = 0.1f;
+
+    [Header("Spells Cost")]
+    public float CreatePlatformCost = 100.0f;
+
+    [Header("Player")]
+    public GameObject Player;
+
+    [Header("Prefabs")]
+    public Transform PlatformPrefab;
+
+    public void Charge()
+    {
+        Energy += ChargeRate;
+    }
+
+    public void CreatePlatform()
+    {
+        if (Energy < CreatePlatformCost)
+            return;
+
+        Energy -= CreatePlatformCost;
+
+        Instantiate(PlatformPrefab, Player.transform.position + Vector3.forward, Quaternion.identity);
+    }
+
+    public void Start()
+    {
+        InvokeRepeating("Charge", 0, 1.0f);
+    }
+
+    public void Update()
+    {
+    }
+}
