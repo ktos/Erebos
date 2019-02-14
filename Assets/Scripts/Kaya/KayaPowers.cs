969fc1bf7480b2200a99514bdf698d243a1565c4
Marcin Badurowicz <m@badurowicz.net>
2019-02-14 23:27:38 +0100

    Add some basic powers for Kaya

---
+++ Assets/Scripts/Kaya/KayaPowers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KayaPowers : MonoBehaviour
+{
+    [Header("Kaya Magic")]
+    public float Energy = 0;
+
+    public float ChargeRate = 0.1f;
+
+    public GameObject InfluenceSphere;
+
+    public float InfluenceSize = 0;
+
+    public float InfluenceSizeGrowthRate = 0.1f;
+
+    [Header("Player")]
+    public GameObject Player;
+
+    private void UpdateInfluenceSize()
+    {
+        InfluenceSphere.transform.localScale = new Vector3(InfluenceSize, InfluenceSize, InfluenceSize);
+    }
+
+    public void Charge()
+    {
+        Energy += ChargeRate;
+        InfluenceSize += InfluenceSizeGrowthRate;
+
+        UpdateInfluenceSize();
+    }
+
+    public void Start()
+    {
+        InvokeRepeating("Charge", 0, 1.0f);
+    }
+}
